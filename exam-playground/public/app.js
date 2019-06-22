8826be7b2e542f681bab43f7ae9f8a6703d1b6da
NaamaBarIlan <barilan.naama@gmail.com>
2019-06-22 09:39:03 -0700

    class demo

--- exam-playground/public/app.js
+++ exam-playground/public/app.js
@@ -1,8 +1,19 @@
+'use strict';
 
 function init() {
     // safe to test jQuery stuff now
+    $('#p1').on('click', function(event){ $(this).text('bye'); }); 
 }
 
 $(() => {
     init();
 });
+
+// what is wrong with this json? see fun.json file.
+// const test = [
+//     {name:'Sneakers', rank:1, type:'chocolate'},
+//     {name:'Reeses', rank:4, type:'peanut'},
+// ];
+
+// console.log (test);
+
